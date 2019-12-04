7b6350789129028300e8f748574ff8bdad5db9dd
qardpeet <patplaysg@gmail.com>
2019-12-04 23:06:49 +0400

    Updates

---
+++ precache-manifest.ed252ae4cb54206193373f23208b57e8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c7e6c5eab81a6c812cd7ae698a166c8",
+    "url": "/customizable-react-tictactoe/index.html"
+  },
+  {
+    "revision": "8633708151ce7865c261",
+    "url": "/customizable-react-tictactoe/static/css/main.b0da670f.chunk.css"
+  },
+  {
+    "revision": "0e7fdce23df4145a48b5",
+    "url": "/customizable-react-tictactoe/static/js/2.224f01cd.chunk.js"
+  },
+  {
+    "revision": "8633708151ce7865c261",
+    "url": "/customizable-react-tictactoe/static/js/main.c06ad109.chunk.js"
+  },
+  {
+    "revision": "28c443b947711df22964",
+    "url": "/customizable-react-tictactoe/static/js/runtime-main.c92adf02.js"
+  }
+]);
